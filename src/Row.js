da7e4cbf4e7b53b384ead66a3698fd093159a691
Markos Vinicius <markoscomks@gmail.com>
2022-05-22 18:19:37 +0100

    Adicted the other rows

--- src/Row.js
+++ src/Row.js
@@ -29,6 +29,7 @@ function Row({ title, fetchUrl }) {
             
             {movies.map(movie =>(
                 <img 
+                key={movie.id}
                 className="row_poster"
                 src={`${base_url}${movie.poster_path}`} 
                 alt={movie.name}
--- src/requests.js
+++ src/requests.js
@@ -5,7 +5,7 @@ const requests = {
     fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
     fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
     fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=028`,
-    fetchComdeyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=035`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=035`,
     fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=027`,
     fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
     fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
