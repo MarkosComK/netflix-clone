9df6d6f446b3279bb04134c1691a2829840f0ddc
Markos Vinicius <markoscomks@gmail.com>
2022-05-22 17:53:21 +0100

    starting with css

--- src/Row.js
+++ src/Row.js
@@ -1,17 +1,40 @@
 import React, { useEffect, useState } from 'react'
 import axios from './axios';
+import './Row.css'
 
-function Row({ title }) {
+const base_url = "https://image.tmdb.org/t/p/original/";
+
+function Row({ title, fetchUrl }) {
     const [movies, setMovies] = useState([])
 
     //A snippet of code wich runs based on a specific condition
     useEffect( ()=> {
         // if [],  run once when the row loads, and don't run again
-    }, [movies])
+        async function fetchData() {
+            const request = await axios.get(fetchUrl);
+            setMovies(request.data.results);
+            console.table(request)
+            return request;
+            //"https://api.themoviedb.org/3"
+
+        }
+        fetchData();
+        }, [fetchUrl])
 
   return (
-    <div>
+    <div className="row">
         <h2>{title}</h2>
+        <div className="row_posters">
+            {/* {several row_poster(s)} */}
+            
+            {movies.map(movie =>(
+                <img 
+                className="row_poster"
+                src={`${base_url}${movie.poster_path}`} 
+                alt={movie.name}
+                />
+            ))}
+        </div>
         {/* {title} */}
         {/* {containert / posters} */}
     </div>
--- src/requests.js
+++ src/requests.js
@@ -1,14 +1,14 @@
 const API_KEY = "436de7473c370dc2c88e6b6a2c28e34b";
 
 const requests = {
-    fetchTrending: `/trending/all/week?api_key${API_KEY}&language=en-US`,
-    fetchNetflixOriginals: `/discover/tv?api_key${API_KEY}&with_networks=213`,
-    fetchTopRated: `/movie/top_rated?api_key${API_KEY}&language=en-US`,
-    fetchActionMovies: `/discover/movie?api_key${API_KEY}&with_genres=028`,
-    fetchComdeyMovies: `/discover/movie?api_key${API_KEY}&with_genres=035`,
-    fetchHorrorMovies: `/discover/movie?api_key${API_KEY}&with_genres=027`,
-    fetchRomanceMovies: `/discover/movie?api_key${API_KEY}&with_genres=10749`,
-    fetchDocumentaries: `/discover/movie?api_key${API_KEY}&with_genres=99`,
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=028`,
+    fetchComdeyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=035`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=027`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
 }
 
 export default requests
