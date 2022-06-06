d310285a5f3582e3106f51eb87d84399371fae8a
Markos Vinicius <markoscomks@gmail.com>
2022-06-06 16:40:40 +0100

    seting the banner

--- src/Banner.js
+++ src/Banner.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from './axios'
+import requests from './Requests'
 
 function Banner() {
+  const [movie, setMovie] = useState([])
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchNetflixOriginals)
+      setMovie(
+        request.data.results[
+        Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      )
+      return request
+    }
+    fetchData()
+  }, [])
+
+  console.log(movie)
+  
   return (
     <div>
-        
+      <header>
+        {/* {title} */}
+        {/* {div > 2 buttons} */}
+        {/* {description} */}
+      </header>
     </div>
   )
 }
